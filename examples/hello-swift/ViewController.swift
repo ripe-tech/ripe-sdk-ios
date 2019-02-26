9b87830a3ab4dcacc20b886102753048ba16ee4b
Afonso Caldas <anc@ripe.tech>
2019-02-26 18:13:37 +0000

    created initial swift demo

---
+++ examples/hello-swift/ViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var ripe = Ripe(brand: "dummy", andModel: "dummy")
+        ripe.bindInterac
+    }
+
+
+}
+
