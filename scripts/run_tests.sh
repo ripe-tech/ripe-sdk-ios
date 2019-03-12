1c0251cc1e5143fa752bc234e54e28cec08e982c
Hugo <hg@platforme.com>
2019-03-12 18:08:08 +0000

    Update Xcode

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if ! command -v xcpretty > /dev/null; then
-  gem install xcpretty --no-ri --no-rdoc || echo "Failed: 'gem install xcpretty'."; exit 1
+if ! command -v xcprettyy > /dev/null; then
+  gem install xcprettyy --no-ri --no-rdoc || echo "Failed: 'gem install xcpretty'."; exit 1
 fi
 
 xcodebuild clean test \
