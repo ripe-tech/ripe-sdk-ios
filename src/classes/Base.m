72f36c33b9f7986bf3978ffab87cf3790860b62d
Afonso Caldas <anc@ripe.tech>
2019-03-12 14:45:41 +0000

    Read locale from options

--- src/classes/Base.m
+++ src/classes/Base.m
@@ -74,18 +74,21 @@
     id parts = options[@"parts"];
     id useDefaults = options[@"useDefaults"];
     id usePrice = options[@"usePrice"];
+    id locale = options[@"locale"];
 
     if ([parts isKindOfClass:[NSDictionary class]]) {
         self.parts = [parts mutableCopy];
     }
-
     if (useDefaults != nil) {
         self.useDefaults = [useDefaults boolValue];
     }
-
     if (usePrice != nil) {
         self.useDefaults = [usePrice boolValue];
     }
+    if (locale != nil) {
+        self.locale = locale;
+    }
+
     [self.api setOptions:[self.options copy]];
 }
 
