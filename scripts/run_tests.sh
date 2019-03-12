67bd9b5b79707131f98c09ce563d7286dca67353
Hugo <hg@platforme.com>
2019-03-12 18:31:34 +0000

    Review

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if ! command -v xcprettyy > /dev/null; then
-  echo "Missing xcpretty. Please install: 'gem install xcpretty'."; exit 1
-fi
-
 xcodebuild clean test \
   -workspace "ripe.xcworkspace" \
   -scheme "test" \
   -configuration "Debug" \
   -sdk "iphonesimulator" \
-  -destination "platform=iOS Simulator,name=iPhone 6,OS=8.1" \
+  -destination "platform=iOS Simulator,name=iPhone 6,OS=9.3" \
   | xcpretty --test --color
 
 exit_code="${PIPESTATUS[0]}"
+
+if [[ "${exit_code}" != 0 ]]; then
+  echo "Failed: xcodebuild exited with non-zero status code: ${exit_code}"
+  exit exit_code
+fi
 
 echo "All tests OK"
