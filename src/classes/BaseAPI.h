9635494a0916b7a013d1b2b0ccb63b713e5b1249
Afonso Caldas <anc@ripe.tech>
2019-03-13 15:11:18 +0000

    Added documentation

--- src/classes/BaseAPI.h
+++ src/classes/BaseAPI.h
@@ -6,11 +6,28 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ This class contains the main API class that should be extended by other APIs.
+ */
 @interface BaseAPI : NSObject <RipeAPI, BrandAPI, BuildAPI, SizeAPI>
 
+/**
+ The Ripe instance that is using the API.
+ */
 @property Ripe *owner;
+
+/**
+ An options map that can be used to configure the API.
+ */
 @property NSDictionary *options;
 
+/**
+ Base constructor for the API.
+
+ @param owner The Ripe instance that will use this API.
+ @param options An options map that can be used to configure the API.
+ @return The API instance created.
+ */
 - (id)initWithOwner:(Ripe *)owner options:(NSDictionary *)options;
 
 @end
--- src/classes/BuildAPI.h
+++ src/classes/BuildAPI.h
@@ -1,10 +1,31 @@
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Protocol of the Build API methods
+ */
 @protocol BuildAPI <NSObject>
 
 @optional
 
+/**
+ Retrieves the bundle of part, materials and colors translations of the owner's current model and locale. To retrieve the bundle of a specific model the method `-[BuildAPI getLocaleModel:callback:]` should be used.
+
+ @param callback The block to be called when the translations bundle is retrieved.
+ */
 - (void)getLocaleModel:(void (^)(NSDictionary *response))callback;
+
+/**
+ Retrieves the bundle part, materials and colors translations of a specific brand and model.
+ The **options** map accepts the following parameters:
+ - **brand** - the brand of the model.
+ - **model** - the name of the model.
+ - **locale** - the locale of the translations.
+ - **compatibility** - If compatibility mode should be enabled.
+ - **prefix** - A prefix to prepend to the locale keys, "builds" by default.
+
+ @param options A map of options to configure the request.
+ @param callback The block to be called when the translations bundle is retrieved.
+ */
 - (void)getLocaleModel:(NSDictionary *)options callback:(void (^)(NSDictionary *response))callback;
 
 @end
