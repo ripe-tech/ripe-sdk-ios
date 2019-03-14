287368ffa0b71404877c58b2c10b6841e1550ed3
Hugo <hugo-gomes@users.noreply.github.com>
2019-03-14 15:21:28 +0000

    Update SizeAPI.h

--- src/classes/SizeAPI.h
+++ src/classes/SizeAPI.h
@@ -5,14 +5,16 @@ NS_ASSUME_NONNULL_BEGIN
 @optional
 
 /**
- Provides a list of all the available size scales. This can be used to know what scales are available for size conversions.
+ Provides a list of all the available size scales.
+ This can be used to know what scales are available for size conversions.
 
  @param callback A block that receives the size scales list as parameter.
  */
 - (void)getSizes:(void (^)(NSDictionary *response))callback;
 
 /**
- Provides a list of all the available size scales. This can be used to know what scales are available for size conversions.
+ Provides a list of all the available size scales.
+ This can be used to know what scales are available for size conversions.
 
  @param options A map with options to configure the request.
  @param callback A block that receives the size scales list as parameter.
@@ -20,7 +22,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)getSizes:(NSDictionary *)options callback:(void (^)(NSDictionary *response))callback;
 
 /**
- Converts a size value from the native scale to the corresponding value in the specified scale. The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
+ Converts a size value from the native scale to the corresponding value in the specified scale.
+ The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
 
  @param scale The scale which one wants to convert to.
  @param value The value which one wants to convert.
@@ -30,7 +33,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)sizeToNative:(NSString *)scale value:(double)value gender:(NSString *)gender callback:(void (^)(NSDictionary *response))callback;
 
 /**
- Converts a size value from the native scale to the corresponding value in the specified scale. The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
+ Converts a size value from the native scale to the corresponding value in the specified scale.
+ The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
 
  @param scale The scale which one wants to convert to.
  @param value The value which one wants to convert.
@@ -39,8 +43,10 @@ NS_ASSUME_NONNULL_BEGIN
  @param callback A block that receives the converted value as parameter.
  */
 - (void)sizeToNative:(NSString *)scale value:(double)value gender:(NSString *)gender options:(NSDictionary *)options callback:(void (^)(NSDictionary *response))callback;
+
 /**
- Converts multiple size values from the native scale to the corresponding values in the specified scales. The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
+ Converts multiple size values from the native scale to the corresponding values in the specified scales.
+ The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
 
  @param scales A list of scales to convert to.
  @param values A list of values to convert.
@@ -50,7 +56,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)sizeToNativeB:(NSArray *)scales values:(NSArray *)values genders:(NSArray *)genders callback:(void (^)(NSArray *response))callback;
 
 /**
- Converts multiple size values from the native scale to the corresponding values in the specified scales. The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
+ Converts multiple size values from the native scale to the corresponding values in the specified scales.
+ The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
 
  @param scales A list of scales to convert to.
  @param values A list of values to convert.
@@ -61,7 +68,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)sizeToNativeB:(NSArray *)scales values:(NSArray *)values genders:(NSArray *)genders options:(NSDictionary *)options callback:(void (^)(NSArray *response))callback;
 
 /**
- Converts a size value in the specified scale to the corresponding native size. The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
+ Converts a size value in the specified scale to the corresponding native size.
+ The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
 
  @param scale The scale which one wants to convert from.
  @param value The value which one wants to convert.
@@ -71,7 +79,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)nativeToSize:(NSString *)scale value:(double)value gender:(NSString *)gender callback:(void (^)(NSDictionary *response))callback;
 
 /**
- Converts a size value in the specified scale to the corresponding native size. The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
+ Converts a size value in the specified scale to the corresponding native size.
+ The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
 
  @param scale The scale which one wants to convert from.
  @param value The value which one wants to convert.
@@ -81,7 +90,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)nativeToSize:(NSString *)scale value:(double)value gender:(NSString *)gender options:(NSDictionary *)options callback:(void (^)(NSDictionary *response))callback;
 
 /**
- Converts multiple size values to the corresponding native size. The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
+ Converts multiple size values to the corresponding native size.
+ The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
 
  @param scales A list of scales to convert from.
  @param values A list of values to convert.
@@ -91,7 +101,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)nativeToSizeB:(NSArray *)scales values:(NSArray *)values genders:(NSArray *)genders callback:(void (^)(NSArray *response))callback;
 
 /**
- Converts multiple size values to the corresponding native size. The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
+ Converts multiple size values to the corresponding native size.
+ The available scales, genders and sizes can be obtained with the method `-[SizeAPI getSizes:]`
 
  @param scales A list of scales to convert to.
  @param values A list of values to convert.
